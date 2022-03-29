a08451e82ffe9ccf6ba7f6f88183bd94240362cd
Lucas Souza <lucas.souza@myorgtech.com>
2022-03-28 22:03:13 -0300

    Ajuste end-point busca por Id

--- src/main/java/com/api/cadastro_dispositivos/controller/DispositivosController.java
+++ src/main/java/com/api/cadastro_dispositivos/controller/DispositivosController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/dispositivos")
@@ -31,7 +30,7 @@ public class DispositivosController {
         return dispositivoService.getAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{deviceId}")
     public Dispositivo getById(@PathVariable("deviceId") Long deviceId){
         return dispositivoService.findById(deviceId);
     }
