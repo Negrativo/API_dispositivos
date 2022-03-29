bf01a13cb6d48c52308312b4447050e4c5ca3f62
Lucas Souza <lucas.souza@myorgtech.com>
2022-03-29 18:31:20 -0300

    Implementado configuração RabbitMQ e envio dispositivos

--- src/main/java/com/api/cadastro_dispositivos/controller/DispositivosController.java
+++ src/main/java/com/api/cadastro_dispositivos/controller/DispositivosController.java
@@ -1,10 +1,14 @@
 package com.api.cadastro_dispositivos.controller;
 
+import com.api.cadastro_dispositivos.conections.MQConfig;
 import com.api.cadastro_dispositivos.dtos.DispositivoDto;
+import com.api.cadastro_dispositivos.dtos.DispositivoListDto;
 import com.api.cadastro_dispositivos.model.Dispositivo;
 import com.api.cadastro_dispositivos.services.DispositivoService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +21,21 @@ public class DispositivosController {
 
     @Autowired
     DispositivoService dispositivoService;
+    @Autowired
+    private RabbitTemplate template;
 
     @PostMapping("/registrar")
     public ResponseEntity<Object> save(@RequestBody @Valid DispositivoDto dispositivoDto) {
         var dispositivo = new Dispositivo();
         BeanUtils.copyProperties(dispositivoDto, dispositivo);
         return dispositivoService.save(dispositivo);
+    }
+
+    @PostMapping("/registrar/lista")
+    public ResponseEntity<Object> saveList(@RequestBody DispositivoListDto dispositivoListDto) {
+        template.convertAndSend(MQConfig.EXCHANGE,
+                MQConfig.ROUTING_KEY, dispositivoListDto);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/listar")
